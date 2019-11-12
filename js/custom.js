2806883b9fe8e32423d758de403d72d5e9d41a1c
PrinceKap <kumahamezugbeprince@gmail.com>
2019-11-12 19:55:29 +0000

    initial files

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+/*************************** 
+       Services 
+***********************/
+$(function(){
+  new WOW().init();  
+});
+/************************
+       home carousel
+*************************/
+$(function(){
+    $('#home .owl-carousel').owlCarousel({
+          items:1,
+        loop:true,
+        autoplay:true,
+        autoplayHoverPause:true,
+        smartSpeed:700,
+    });
+});
+       //..........................
+       //         products
+       //..........................
+
+
+
+$(document).ready(function(){
+
+$('.title-1').click(function(){
+$('#item-details-1').toggle('slow');
+
+});
+
+});                  
+    
+    
+$(document).ready(function(){
+
+$('.title-2').click(function(){
+$('#item-details-2').toggle('slow');
+
+});
+
+});                  
+                    
+    
+$(document).ready(function(){
+
+$('.title-3').click(function(){
+$('#item-details-3').toggle('slow');
+
+});
+
+});                  
+    
+$(document).ready(function(){
+
+$('.title-4').click(function(){
+$('#item-details-4').toggle('slow');
+
+});
+
+});                  
+    
